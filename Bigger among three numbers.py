71b949fc4d3c71457ab6fb5cec6e770a084f5532
Ajit kumar <97993300+Ajitkumar12915@users.noreply.github.com>
2022-09-24 19:27:07 +0530

    Update Bigger among three numbers.py

--- Bigger among three numbers.py
+++ Bigger among three numbers.py
@@ -7,3 +7,16 @@ if max<b:
     if max<c:
         max=c
         print('Bigger=',max)
+        # another method for same problem is given below.
+      a=int(input("Enter first number="))
+b=int(input("Enter second number="))
+c=int(input("Enter third number="))
+if a>b:
+    if a>c:
+        print(a)
+       else:
+        if b>c:
+            print(b)
+            else:
+                print(c)
+        
